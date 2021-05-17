edcf9f0fac90ddecc4387d8ca76c338d6d5648f1
Dell <Dell@DESKTOP-I75CK6E>
2021-05-17 09:13:27 +0530

    Updated

--- src/javaPackage/ArithematicOperator.java
+++ src/javaPackage/ArithematicOperator.java
@@ -5,7 +5,7 @@ public class ArithematicOperator {
 	public static void main(String[] args) {
 		 
 		int a=30; // local variable 
-		int b=20; // local variable 
+		int b=50; // local variable 
 		
 		System.out.println(a+b); //40
 		System.out.println(a-b); //0
